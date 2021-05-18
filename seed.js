f46dd96f971d325f7b13e18c8d0c082141b88e45
prince-bigstep <82824639+prince-bigstep@users.noreply.github.com>
2021-05-18 11:15:53 +0530

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,19 +1,12 @@
-// connect db
-// check first user 
-// if not username = admin:
-// random password(Rand)
-// user.register(details)
-// console.credentials(user, pas)
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+} 
 
 const mongoose = require('mongoose');
 const User = require('./models/user');
 const Password = require('secure-random-password');
-
-mongoose.connect('mongodb://localhost:27017/hr1', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+const MongoStore = require("connect-mongo");
+const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/hr1';
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -21,8 +14,14 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
-const seedDB = async (req,res) => {
+const seedDB = async () => {
+    await User.deleteMany({});
     const firstUser = await User.findOne({username : "admin"});
     if (!firstUser){
         const password = Password.randomPassword({ length: 8, characters: [Password.lower, Password.upper, Password.digits] })
@@ -33,8 +32,9 @@ const seedDB = async (req,res) => {
             email: 'bigstep@gmail.com',
         });
         const registerdUser = await User.register(user, password);
-        // console.log("registerUser", registerdUser)
+        console.log("registerUser", registerdUser)
         console.log(`Credentials=>  Username: ${username} Password ${password}`)
+
     }
     else{
         console.log("firstUser present", firstUser)
